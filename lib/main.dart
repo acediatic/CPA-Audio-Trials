0261a5d7ed0874e82db6b6fbe18a43b17826f050
Acediatic <hello@adamsinclair.kiwi>
2022-05-29 18:07:38 +1200

    feat: completes speed trials

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'dart:io';
-
-import 'package:cpa_demo_app/stopwatch.dart';
+import 'package:cpa_demo_app/sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:intl/intl.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -29,130 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const AudioSpeedTrialPage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class AudioSpeedTrialPage extends StatefulWidget {
-  const AudioSpeedTrialPage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<AudioSpeedTrialPage> createState() => _AudioSpeedTrialPageState();
-}
-
-class _AudioSpeedTrialPageState extends State<AudioSpeedTrialPage> {
-  static const maxRandomInt = 637;
-  int randomNumber = 0;
-  String saFileName = "";
-  Stopwatch stopwatch = Stopwatch();
-  bool isRunning = false;
-
-  String formatNumber(int number) {
-    return NumberFormat("000", 'en_US').format(number);
-  }
-
-  void updateRandomNumber() {
-    setState(() {
-      randomNumber = Random().nextInt(maxRandomInt);
-      saFileName = formatNumber(randomNumber) + "SA.mp3";
-    });
-  }
-
-  void startSearch() {
-    stopwatch.reset();
-    stopwatch.start();
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void endSearch() {
-    stopwatch.stop();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text("Source:", style: Theme.of(context).textTheme.headline2),
-            FractionallySizedBox(
-              widthFactor: 0.8,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  OutlinedButton(
-                      onPressed: updateRandomNumber,
-                      child: const Text("Generate Random Number")),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Text(formatNumber(randomNumber)),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () => {
-                    startSearch(),
-                    endSearch(),
-                  },
-                  child: const Text("Begin Search"),
-                  style: TextButton.styleFrom(
-                    backgroundColor:
-                        isRunning ? Colors.yellow : Colors.transparent,
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: SearchStopwatch(time: stopwatch.elapsedMilliseconds),
-                ),
-              ],
-            ),
-            FractionallySizedBox(
-              widthFactor: 0.8,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black),
-                ),
-                child: Center(
-                  child: Text(
-                    "Audio file: " + saFileName,
-                    style: const TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const SignInPage(),
     );
   }
 }
---
+++ lib/show_error.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void showError(BuildContext context, String e) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Error'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(e),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
