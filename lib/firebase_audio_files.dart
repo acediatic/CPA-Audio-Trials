519c9af8b86a9258c887237d7d7857c91a926232
Acediatic <hello@adamsinclair.kiwi>
2022-05-29 18:07:02 +1200

    feat: adds firebase

---
+++ lib/firebase_audio_files.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseAudio {
+  final storage = FirebaseStorage.instance;
+  final storageRef = FirebaseStorage.instance.ref();
+  static final FirebaseAudio _singleton = FirebaseAudio._internal();
+
+  factory FirebaseAudio() {
+    return _singleton;
+  }
+  FirebaseAudio._internal();
+
+  Future<String> _getAudioFromString(String filePath) async {
+    final pathReference = storageRef.child(filePath);
+    print("path ref: ${pathReference.fullPath}");
+    print("path ref2: ${await pathReference.getDownloadURL()}");
+
+    return pathReference.getDownloadURL();
+  }
+
+  Future<String> getSAudioFromPath(String filePath) {
+    return _getAudioFromString("S/$filePath");
+  }
+}
