eb60580f10125214c276d48cb93c449e6a6c415c
Acediatic <hello@adamsinclair.kiwi>
2022-05-29 18:07:22 +1200

    feat: adds sign in page and updates audio speed trial page

---
+++ lib/audio_speed_trial.dart
@@ -0,0 +1,124 @@
+import 'package:cpa_demo_app/firebase_audio_files.dart';
+import 'package:cpa_demo_app/stopwatch.dart';
+import 'dart:math';
+import 'package:intl/intl.dart';
+import 'package:audioplayers/audioplayers.dart';
+import "package:flutter/material.dart";
+import 'show_error.dart';
+
+class AudioSpeedTrialPage extends StatefulWidget {
+  const AudioSpeedTrialPage({Key? key}) : super(key: key);
+  @override
+  State<AudioSpeedTrialPage> createState() => _AudioSpeedTrialPageState();
+}
+
+class _AudioSpeedTrialPageState extends State<AudioSpeedTrialPage> {
+  static const maxRandomInt = 133;
+  int randomNumber = 0;
+  String saFileName = "";
+  Stopwatch stopwatch = Stopwatch();
+  bool isRunning = false;
+  AudioPlayer audioPlayer = AudioPlayer();
+  FirebaseAudio firebaseAudio = FirebaseAudio();
+
+  String formatNumber(int number) {
+    return NumberFormat("000", 'en_US').format(number);
+  }
+
+  void updateRandomNumber() {
+    setState(() {
+      randomNumber = Random().nextInt(maxRandomInt);
+      saFileName = "${formatNumber(randomNumber)}SA.mp3";
+      print(saFileName);
+    });
+  }
+
+  void startSearch() {
+    stopwatch.reset();
+    stopwatch.start();
+    setState(() {
+      isRunning = true;
+    });
+
+    firebaseAudio.getSAudioFromPath(saFileName).then((audioUrl) {
+      return audioPlayer.play(audioUrl);
+    }).catchError((e) {
+      showError(context, e.toString());
+    }).whenComplete(endSearch);
+  }
+
+  void endSearch() {
+    stopwatch.stop();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text("Source:", style: Theme.of(context).textTheme.headline2),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton(
+                      onPressed: updateRandomNumber,
+                      child: const Text("Generate Random Number")),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Text(formatNumber(randomNumber)),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: startSearch,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isRunning ? Colors.yellow : Colors.transparent,
+                  ),
+                  child: const Text("Begin Search"),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: SearchStopwatch(time: stopwatch.elapsedMilliseconds),
+                ),
+              ],
+            ),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Center(
+                  child: Text(
+                    "Audio file: $saFileName",
+                    style: const TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/sign_in.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'audio_speed_trial.dart';
+import "show_error.dart";
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+// simple sign in page with google or email and password
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Sign in with Google'),
+              onPressed: () {
+                signInWithGoogle().then((_) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AudioSpeedTrialPage()),
+                  );
+                }).catchError((e) {
+                  showError(context, e);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
